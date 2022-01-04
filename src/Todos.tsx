aeb463d5f76d3994d3049d8d516c0a23e32a57c2
Maher Hussain <maherhussain6@gmail.com>
2022-01-04 22:51:50 +0100

    changes done

--- src/Todos.tsx
+++ src/Todos.tsx
@@ -1,86 +1,151 @@
-import { Component } from "react";
+import React, { useState } from "react";
 
-export default class Todos extends Component {
-  state = {
-    newTodo: "",
-    todos: ["hello there", "general kenobi"],
-  };
+export default function Todos() {
+  const todosList = [
+    { id: 1, title: "hello there", deadline: "", status: "" },
+    { id: 2, title: "general kenobi", deadline: "", status: "" },
+  ];
 
-  onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  // eslint-disable-next-line @typescript-eslint/no-redeclare
+
+  const [todos, setTodos] = useState(todosList);
+  const [title, setTitle] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!this.state.newTodo) return;
-
-    const todos = [this.state.newTodo, ...this.state.todos];
-    this.setState({ newTodo: "", todos });
+    if (!title) return;
+    const newTodo = {
+      id: Math.random(),
+      title: title,
+      deadline: "",
+      status: "",
+    };
+    setTodos(todos.concat(newTodo));
+    setTitle("");
   };
 
-  removeTodo = (removeIndex: number) => {
-    const todos = this.state.todos.filter((_, index) => index !== removeIndex);
-    this.setState({ todos });
+  const removeTodo = (removeIndex: number) => {
+    const removedItem = todos.filter((_, index) => index !== removeIndex);
+    setTodos(removedItem);
+  };
+  const deadlineHandler = (e: { target: { value: any } }, id: number) => {
+    const deadlineValue = e.target.value;
+
+    const todoWithDeadline = todos.map((item) => {
+      if (item.id === id) {
+        item.deadline = deadlineValue;
+      }
+      return item;
+    });
+
+    setTodos(todoWithDeadline);
   };
 
-  render() {
-    return (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: 500,
-          margin: "0 auto",
-          padding: 8,
-        }}
-      >
-        <h2 style={{ textAlign: "center" }}>Todo</h2>
-        <form
-          onSubmit={this.onSubmit}
-          style={{ display: "flex", marginBottom: 8 }}
+  const onChange = (e: { target: { name: any; value: any } }, id: number) => {
+    const { value } = e.target;
+
+    const selectedTodo = todos.map((item) => {
+      if (item.id === id) {
+        item.status = value;
+      }
+      return item;
+    });
+
+    setTodos(selectedTodo);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: 1000,
+        margin: "0 auto",
+        padding: 8,
+      }}
+    >
+      <h2 style={{ textAlign: "center" }}>Todo</h2>
+      <form onSubmit={onSubmit} style={{ display: "flex", marginBottom: 8 }}>
+        <input
+          type="text"
+          name="newTodo"
+          id="newTodo"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Fix the thing.."
+          style={{
+            display: "inline-flex",
+            flex: 1,
+            padding: 4,
+            border: "1px solid #eaeaea",
+            marginRight: 4,
+          }}
+        />
+        <button
+          type="submit"
+          style={{ borderColor: "#eaeaea", backgroundColor: "#fff" }}
         >
-          <input
-            type="text"
-            name="newTodo"
-            id="newTodo"
-            value={this.state.newTodo}
-            onChange={(e) => this.setState({ newTodo: e.target.value })}
-            placeholder="Fix the thing.."
-            style={{
-              display: "inline-flex",
-              flex: 1,
-              padding: 4,
-              border: "1px solid #eaeaea",
-              marginRight: 4,
-            }}
-          />
-          <button
-            type="submit"
-            style={{ borderColor: "#eaeaea", backgroundColor: "#fff" }}
-          >
-            Add
-          </button>
-        </form>
-        <div>
-          {this.state.todos.length === 0 && (
-            <div style={{ textAlign: "center" }}>Add some todos</div>
-          )}
-          {this.state.todos.map((todo, i) => (
+          Add
+        </button>
+      </form>
+      <div>
+        {todos.length === 0 && (
+          <div style={{ textAlign: "center" }}>Add some todos</div>
+        )}
+
+        {todos &&
+          todos.map((item, i) => (
             <div
-              key={`${todo}-${i}`}
+              key={`${item.title}-${i}`}
               style={{
                 padding: 4,
                 borderBottom: "1px solid #ccc",
                 display: "flex",
               }}
             >
-              <span style={{ flex: 1 }}>{todo}</span>
-              <span
-                style={{ cursor: "pointer" }}
-                onClick={() => this.removeTodo(i)}
-              >
+              <span style={{ flex: 1 }}>{item.title}</span>
+              <span style={{ flex: 1 }}>{item.status}</span>
+              <span style={{ flex: 1 }}>{item.deadline}</span>
+              <div style={{ flex: 1 }}>
+                todo
+                <input
+                  type="radio"
+                  onChange={(e) => onChange(e, item.id)}
+                  name={`${item.title}${item.id}`}
+                  value="todo"
+                />
+                doing{" "}
+                <input
+                  type="radio"
+                  onChange={(e) => onChange(e, item.id)}
+                  name={`${item.title}${item.id}`}
+                  value="doing"
+                />
+                    done
+                <input
+                  type="radio"
+                  onChange={(e) => onChange(e, item.id)}
+                  name={`${item.title}${item.id}`}
+                  value="done"
+                />
+                 
+              </div>
+
+              <span style={{ cursor: "pointer" }} onClick={() => removeTodo(i)}>
                 &times;
+              </span>
+              <span style={{ margin: "0 20px" }}>
+                <input
+                  type="date"
+                  name={`${item.title}${item.id}`}
+                  min={Date.now()}
+                  max="2022-12-31"
+                  onChange={(e) => deadlineHandler(e, item.id)}
+                />
               </span>
             </div>
           ))}
-        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
